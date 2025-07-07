26197dff1f6fcc328021e8509ea0fb31fded4cca
Modenjaya <107100725+Modenjaya@users.noreply.github.com>
2025-07-07 19:14:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,5 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 SOLANA_RPC_URL = os.getenv("SOLANA_RPC_URL", "https://api.mainnet-beta.solana.com")
-JUPITER_API_URL = "https://quote-api.jup.ag/v6"
-DATABASE_PATH = "bot_database.db"
-ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")  # Untuk enkripsi private key
+COINVERA_API_KEY = os.getenv("COINVERA_API_KEY")  # API key CoinVera
+COPY_WALLET = os.getenv("COPY_WALLET")            # Wallet yang ingin dicopy
